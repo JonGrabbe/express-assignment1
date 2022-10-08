cd59f6d51715916c0164eade7f525b5d2a739158
JonGrabbe <jgrabbe69@gmail.com>
2022-10-08 16:37:16 -0700

    step 2

--- app.js
+++ app.js
@@ -1,8 +1,16 @@
 const express = require('express');
 const app = express();
 
+
+app.use((req, res, next) => {
+    // res.send('<h1>About us page</h1>')
+    console.log('first log about page')
+    next()
+})
+
 app.use((req, res, next) => {
     res.send('<h1>hello world hahaah</h1>')
+    console.log('second log')
 })
 
 app.listen(3000)
