ffd7eba571af2c3ef0b978f8bb2b5b5acdb6959f
JonGrabbe <jgrabbe69@gmail.com>
2022-10-08 17:05:41 -0700

    logged body using bodyParser package ;

--- app.js
+++ app.js
@@ -1,15 +1,30 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({extended: false}))
 
 
-app.use((req, res, next) => {
+let htmlForm = `
+    <form action="/product" method="POST">
+        <input type="text" name="title">
+        <button type="submit">send</button>
+    </form>
+`;
+
+app.use('/add-product', (req, res, next) => {
     // res.send('<h1>About us page</h1>')
-    console.log('first log about page')
-    next()
+    console.log(req.body)
+    res.send(htmlForm)
+})
+app.use('/product', (req, res, next) => {
+    // res.send('<h1>About us page</h1>')
+    console.log(req.body)
+    res.send('<h1>Products</h1>')
 })
 
 app.use((req, res, next) => {
-    res.send('<h1>hello world hahaah</h1>')
+    res.send('<h1>Home</h1>')
     console.log('second log')
 })
 
