c78e8af917dadceab8a17405962796cd7cfc5ca9
JonGrabbe <jgrabbe69@gmail.com>
2022-10-21 20:44:17 -0700

    logged product array

--- about.js
+++ about.js
@@ -10,25 +10,16 @@ let htmlForm = `
     </form>
 `;
 
-const products = [{testing: 'hello there'}]
-
 router.get('/add-product', (req, res, next) => {
     // res.send('<h1>About us page</h1>')
     // res.send(htmlForm)
-    // products.push({a: req.body.title})
     res.sendFile(path.join(rootDir, 'views', 'products.html'))
-})
-router.post('/add-product', (req, res, next) => {
-    products.push({productTitle: req.body.title})
-    res.redirect('/')
 })
 router.post('/product', (req, res, next) => {
     // res.send('<h1>About us page</h1>')
-    // console.log(req.body)
+    console.log(req.body)
     res.send('<h1>Products</h1>')
     // res.send(`<h2>${req.body.title}</h2>`)
 })
 
-// module.exports = router;    
-exports.routes = router
-exports.products = products
+module.exports = router;
--- routes/shop.js
+++ routes/shop.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const router = express.Router();
+const routes = require('./admin')
 
 router.get('/', (req, res, next) => {
     res.send('<h1>Home</h1>')
+    console.log(routes.products)
     console.log('second log')
 })
 
